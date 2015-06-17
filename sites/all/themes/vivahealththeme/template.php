3f2775c9c035e093280bd7ed56da803c22004a63
Rhiannon <rhiannon@viva.org.uk>
2015-06-17 10:41:35 +0100

    full site

--- sites/all/themes/vivahealththeme/template.php
+++ sites/all/themes/vivahealththeme/template.php
@@ -5,3 +5,5 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * vivahealththeme theme.
  */
+drupal_add_library('system', 'ui.accordion');
+drupal_add_js('jQuery(document).ready(function() {jQuery("#accordion").accordion();});','inline');
